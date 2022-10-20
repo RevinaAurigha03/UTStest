863d036499b2ac055471b1300ca79200fa8038e2
RevinaAurigha03 <firdausrerevina@gmail.com>
2022-10-21 00:31:21 +0700

    UTSSMT3

---
+++ Prak5/no.1.php
@@ -0,0 +1,73 @@
+<!-- Revina Aurigha Firdaus -->
+<?php
+class Pegawai
+{
+    public $name;
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+    public function getName()
+    {
+        return $this->name;
+
+    }
+}
+class Manager extends Pegawai
+{
+    public $tunjangan;
+    public function __construct($name, $tunjangan)
+    {
+        parent::__construct($name);
+        $this->tunjangan = $tunjangan;
+    }
+    public function getTunjangan()
+    {
+        return $this->tunjangan;
+    }
+}
+class Kurir extends Pegawai
+{
+    public $gaji;
+    public function __construct($name, $gaji)
+    {
+        parent::__construct($name);
+        $this->gaji = $gaji;
+    }
+    public function getGaji()
+    {
+        return $this->gaji;
+    }
+}
+class SoalNo1
+{
+    public static
+    function Proses($peg)
+    {
+        if ($peg instanceof Manager)
+        {
+            $man = $peg;
+            echo "<br>Nama Manager: ".$man->name, "\n";
+            echo "<br>Tunjangan: RP. ".strval($man->tunjangan), "\n";
+        }
+        else if ($peg instanceof Kurir)
+        {
+            $kur = $peg;
+            echo "<br>Nama Kurir: ".$kur->name, "\n";
+            echo "<br>Gaji= RP. ".strval($kur->gaji), "\n";
+        }
+    }
+    public static
+    function main($args)
+    {
+        echo "PRAKTIKUM 5 POLIMORFISME", "\n";
+        echo "<br>", "<br>";
+        $peg1 = new Manager("Revina Aurigha Firdaus", 1200000);
+        SoalNo1::Proses($peg1);
+        echo "<br>", "<br>";
+        $peg2 = new Kurir("Andika T", 2500000);
+        SoalNo1::Proses($peg2);
+    }
+}
+SoalNo1::main(array());
+?>
---
+++ Prak5/no.2.php
@@ -0,0 +1,69 @@
+<!-- Revina Aurigha Firdaus -->
+
+<?php
+class Pegawai
+{
+    public $name;
+    public $gaji;
+    public function __construct($name, $gaji)
+    {
+        $this->name = $name;
+        $this->gaji = $gaji;
+    }
+    public function infoGaji()
+    {
+        return $this->gaji;
+    }
+}
+class Manager extends Pegawai
+{
+    private $tunjangan;
+    public function __construct($name, $gaji, $tunjangan)
+    {
+        parent::__construct($name, $gaji);
+        $this->tunjangan = $tunjangan;
+    }
+    public function infoGaji()
+    {
+        return $this->gaji;
+    }
+    public function infoTunjangan()
+    {
+        return $this->tunjangan;
+    }
+}
+class Programmer extends Pegawai
+{
+    private $bonus;
+    public function __construct($name, $gaji, $bonus)
+    {
+        parent::__construct($name, $gaji);
+        $this->bonus = $bonus;
+    }
+    public function infoBonus()
+    {
+        return $this->bonus;
+    }
+}
+class Bayaran
+{
+    public function hitungBayaran($peg)
+    {
+        $uang = $peg->infoGaji();
+
+        return $uang;
+    }
+    public static function main($args)
+    {
+
+        echo "PRAKTIKUM 5 POLIMORFISME NO.2", "\n";
+        echo "<br>", "<br>";
+        $man = new Manager("Revina A F", 25000000, 45);
+        $prog = new Programmer("Alvaro", 5000000, 30);
+        $hr = new Bayaran();
+        echo "<br>Gaji Manager ". $man->name." : RP. ".strval($hr->hitungBayaran($man)), "\n";
+        echo "<br>Gaji Programmer ". $prog->name." : RP. ".strval($hr->hitungBayaran($prog)), "\n";
+    }
+}
+Bayaran::main(array());
+?>
